421a306a3d49e84e9c9b8963477b8a8fdb338315
Nikki Thean <nikki.thean@gmail.com>
2014-01-20 17:01:18 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApartmentHunter::Application.config.secret_key_base = '03ff4e5606becd7f7134b6f6ff0d479012d2a04c3f56eadc55a7edf0132c0d212a02e994586644bc489790483cf98cf23e7d65cc31dc52960fc0abbc384a90d6'
